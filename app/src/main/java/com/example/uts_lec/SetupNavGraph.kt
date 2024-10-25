e1f8b2aaed185d94c50c6504fbe74fd0c7e1a215
owencahyadi <owenchristian123@gmail.com>
2024-10-25 16:00:22 +0700

    update gabungan

--- app/src/main/java/com/example/uts_lec/SetupNavGraph.kt
+++ app/src/main/java/com/example/uts_lec/SetupNavGraph.kt
@@ -18,5 +18,45 @@ fun SetupNavGraph(navController: NavHostController) {
         composable("Badan") {HeightAndWeightSelectionScreen(navController = navController) }
         composable("Goal") {GoalSelectionScreen(navController = navController) }
         composable("Perkenalan") {IntroductionScreen(navController = navController) }
+        composable("workout") { WorkoutCategoryScreen(navController = navController) }
+        composable("workout_detail_screen/{day}") { backStackEntry ->
+            val day = backStackEntry.arguments?.getString("day")?.toIntOrNull() ?: 1
+            WorkoutDetailScreen(navController = navController, day = day)
+        }
+        composable("workout_days_screen/{category}") { backStackEntry ->
+            val category = backStackEntry.arguments?.getString("category")
+            WorkoutDaysScreen(navController = navController, category = category ?: "Unknown")
+        }
+        composable("tutorial_screen/{exerciseNumber}") { backStackEntry ->
+            val exerciseNumber = backStackEntry.arguments?.getString("exerciseNumber")?.toInt() ?: 1
+            TutorialScreen(navController = navController, exerciseNumber = exerciseNumber)
+        }
+        composable("transition_screen/{exerciseNumber}/{workoutName}") { backStackEntry ->
+            val exerciseNumber = backStackEntry.arguments?.getString("exerciseNumber")?.toInt() ?: 1
+            val workoutName = backStackEntry.arguments?.getString("workoutName") ?: "Unknown"
+            TransitionWorkoutScreen(navController = navController, exerciseNumber = exerciseNumber, workoutName = workoutName)
+        }
+
+        composable("workout_screen/{exerciseNumber}/{name}/{reps}/{imageRes}/{duration}") { backStackEntry ->
+            val exerciseNumber = backStackEntry.arguments?.getString("exerciseNumber")?.toInt() ?: 1
+            val name = backStackEntry.arguments?.getString("name") ?: "Unknown"
+            val reps = backStackEntry.arguments?.getString("reps") ?: ""
+            val imageRes = backStackEntry.arguments?.getString("imageRes")?.toInt() ?: R.drawable.chest_workout
+            val duration = backStackEntry.arguments?.getString("duration")?.toInt() ?: 0
+
+            val workout = Workout(
+                exerciseNumber = exerciseNumber,
+                name = name,
+                reps = reps,
+                imageRes = imageRes,
+                duration = duration
+            )
+
+            WorkoutScreen(navController = navController, workout = workout)
+        }
+        composable("profile") { ProfileScreen(navController) }
+        composable("updateProfile") { UpdateProfileScreen(navController) }
+        composable("settings") { SettingsScreen(navController) }
+        composable("password") { PasswordScreen(navController) }
     }
 }
